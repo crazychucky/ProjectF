0d2420a9b9568503fe9596b1613aa965b0a7941a
yuanlixiao <yuanlixiao@live.cn>
2014-08-31 00:17:38 +0800

    modify screen size

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -18,7 +18,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
 
     pDirector->setOpenGLView(pEGLView);
-	
+	pEGLView->setDesignResolutionSize(800, 480, kResolutionShowAll);
+
     // turn on display FPS
     pDirector->setDisplayStats(true);
 
